ff35aa98f463e324e37a77fd7af695d871cd836f
mukeshswamy <59552894+mukeshswamy@users.noreply.github.com>
2020-05-26 18:34:40 +0530

    modal-added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Button, Table } from "react-bootstrap";
+import "./App.css";
+import ModalHook from "./ModalHook";
+import EditModal from "./EditModal";
 
 function App() {
+  const [modalOpen, isModalOpen] = useState(false);
+  const [modalOpenEdit, isModalOpenEdit] = useState(false);
+
+  const handleModalOpen = () => {
+    isModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    isModalOpen(false);
+  };
+
+  const handleOpenEditModal = () => {
+    isModalOpenEdit(true);
+  };
+
+  const handleCloseEditModal = () => {
+    isModalOpenEdit(false);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container>
+        <div className="head">
+          <h1>PRODUCT TABLE</h1>
+        </div>
+        <div className="addProductBtnDiv">
+          <Button variant="btn btn-primary" onClick={() => handleModalOpen()}>
+            ADD PRODUCT
+          </Button>
+        </div>
+        <Table striped bordered hover variant="dark">
+          <thead>
+            <tr>
+              <th>Sr.No</th>
+              <th>Product Name</th>
+              <th>Product Price</th>
+              <th>SKU</th>
+              <th>Quantity Available</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Bag</td>
+              <td>100</td>
+              <td>1FER321GG</td>
+              <td>4</td>
+              <td>
+                <Button
+                  variant="btn btn-success cusBtnEdit"
+                  onClick={() => handleOpenEditModal()}
+                >
+                  Edit
+                </Button>
+                <Button variant="btn btn-danger">Delete</Button>
+              </td>
+            </tr>
+          </tbody>
+        </Table>
+      </Container>
+      <ModalHook show={modalOpen} onHide={() => handleCloseModal()} />
+      <EditModal show={modalOpenEdit} onHide={() => handleCloseEditModal()} />
     </div>
   );
 }
---
+++ src/EditModal.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { Modal, Button, Form } from "react-bootstrap";
+
+function EditModal(props) {
+  return (
+    <React.Fragment>
+      <div id="myModal" className={`cusmodal ${props.show ? "disp" : ""}`}>
+        <div className="cusmodal-content">
+          <span className="close" onClick={props.onHide}>
+            x
+          </span>
+          <h3>Edit Product</h3>
+          <hr />
+          <div>
+            <Modal.Body>
+              <Form>
+                <Form.Group controlId="formBasicName">
+                  <Form.Label>Product Name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Product Name"
+                    name="productName"
+                  />
+                </Form.Group>
+                <Form.Group controlId="formBasicPrice">
+                  <Form.Label>Product Price</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Product Price"
+                    name="productPrice"
+                  />
+                </Form.Group>
+                <Form.Group controlId="formBasicSKU">
+                  <Form.Label>SKU</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="SKU"
+                    name="productSKU"
+                  />
+                </Form.Group>
+                <Form.Group controlId="formBasicquantity">
+                  <Form.Label>Product Quantity</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Product Quantity"
+                    name="productQuantity"
+                  />
+                </Form.Group>
+              </Form>
+            </Modal.Body>
+          </div>
+          <div>
+            <Modal.Footer>
+              <Button variant="btn btn-primary">Save</Button>
+              <Button variant="btn btn-danger" onClick={props.onHide}>
+                Close
+              </Button>
+            </Modal.Footer>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default EditModal;
